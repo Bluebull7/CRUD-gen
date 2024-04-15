aed42d3319e143588ce3b3c44333ab4c35e4d1b2
James H. <lsaucyfx@gmail.com>
2024-04-15 16:34:04 -0400

    updated model generator

--- src/model-generation/model-generator.ts
+++ src/model-generation/model-generator.ts
@@ -1,9 +1,52 @@
-function generateTypeScriptModels(apiDoc: any): string {
+import { OpenAPI, ReferenceObject, SchemaObject } from 'swagger-parser';
 
-	// mock function to generate TS models from API documentation
-	return "// TypeScript models generated from API documentation";
-};
+interface APIResource {
+	name: string;
+	properties: Property[];
+	// other properties
+}
 
-export { generateTypeScriptModels };
+interface Property {
+	name: string;
+	type: string;
+	isOptional: boolean;
+}
+
+class ModelGenerator {
+	private api: OpenAPi.Document;
+
+	constructor(apispec: OpenAPI.Document) {
+		this.api = apispec;
+	}
+public generateModels(): APIResource[] {
+	const resources: this.extractResources();
+	//Transform resources into APIResource objects
+	return resources;
+}
+
+private extractResources(): SchemaObject[] {
+	const resources: SchemaObject[] = [];
+	Object.keys(this.api.components.schemas).forEach((key) => {
+		const schema = this.api.components.schemas[key];
+		if (this.isResource(schema)) {
+			resources.push(schema);
+		}
+	});
+	return resources;
+}
+
+private isResource(schema: SchemaObject): boolean {
+	return schema.type === 'object';
+}
+
+private mapType(schema: SchemaObject | ReferenceObject): string {
+	if (schema.type === 'array') {
+		return `Array<${this.mapType(schema.items)}>`;
+	} else if (schema.type === 'object') {
+		return 'object';
+	} else {
+		return schema.type;
+	}
+}
 
 
